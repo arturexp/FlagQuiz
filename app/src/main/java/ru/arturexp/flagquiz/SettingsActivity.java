b99e1eda3d78ba3abd514b2d48cd2d22dcc1f63e
arturexp <arturexp@gmail.com>
2016-08-22 14:00:55 +0700

    edit manifest

--- app/src/main/java/ru/arturexp/flagquiz/SettingsActivity.java
+++ app/src/main/java/ru/arturexp/flagquiz/SettingsActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 public class SettingsActivity extends AppCompatActivity {
-    // inflates the GUI, displays Toolbar and adds "up" button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

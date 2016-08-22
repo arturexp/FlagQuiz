0436664c6e32b82845fb85283204f8bccfda3cf2
arturexp <arturexp@gmail.com>
2016-08-22 13:57:33 +0700

    Added SettingsActivity

--- app/src/main/java/ru/arturexp/flagquiz/SettingsActivity.java
+++ app/src/main/java/ru/arturexp/flagquiz/SettingsActivity.java
@@ -1,22 +1,16 @@
 package ru.arturexp.flagquiz;
-
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
-
+    // inflates the GUI, displays Toolbar and adds "up" button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
 }
